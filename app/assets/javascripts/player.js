cbd28daa525b002f571a171a63dcf81835269119
Dani Dirks & Matthew Meyers <pair+danisaurus.marinatedpork@devbootcamp.com>
2014-10-30 11:15:43 -0500

    changed Asteria class to be model with an instance of a view sprite

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,20 +1,20 @@
-Asteria = function(game, x, y) {
-  Phaser.Sprite.call(this, game, x, y, 'dude');
+var Asteria = function(game, x, y) {
+  this.sprite = game.add.sprite(x, y, 'dude');
 }
+
 // Create functions
-Asteria.prototype = Object.create(Phaser.Sprite.prototype);
-Asteria.prototype.constructor = Asteria;
+
+// Asteria.prototype = Object.create(Phaser.Sprite.prototype);
 
 Asteria.prototype.entersTheScene = function() {
-  this.body.bounce.y = 0.2;
-  this.body.gravity.y = 300;
-  this.body.collideWorldBounds = true;
-
+  this.sprite.body.bounce.y = 0.2;
+  this.sprite.body.gravity.y = 300;
+  this.sprite.body.collideWorldBounds = true;
 }
 
 Asteria.prototype.setMotions = function() {
-  this.animations.add('left', [0, 1, 2, 3], 10, true);
-  this.animations.add('right', [5, 6, 7, 8], 10, true);
+  this.sprite.animations.add('left', [0, 1, 2, 3], 10, true);
+  this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
 }
 
 // Update functions
