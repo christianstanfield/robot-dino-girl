46372f6ca3c4bc82ce4ae08b81480ba5cc23e387
Dani Dirks <dani.dirks@build.com>
2014-10-31 15:51:18 -0500

    made tilemapper more dynamic

--- app/assets/javascripts/tilemapper.js
+++ app/assets/javascripts/tilemapper.js
@@ -4,16 +4,16 @@ var TileMapper = function(game, csvMap, tileSheet) {
   this.sheet = tileSheet
 }
 
-TileMapper.prototype.loadAssets = function() {
-  this.game.load.image('tiles', this.sheet);
-  this.game.load.tilemap('map', this.csvMap, null, Phaser.Tilemap.CSV);
+TileMapper.prototype.loadAssets = function(tilesheet_name, map_name) {
+  this.game.load.image(image_name, this.sheet);
+  this.game.load.tilemap(map_name, this.csvMap, null, Phaser.Tilemap.CSV);
 }
 
 // Input an array of index ranges (i.e. (2,10)) for which you would like the layer to collide.
 
-TileMapper.prototype.createTiles = function(collision_ranges) {
-  this.map = this.game.add.tilemap('map', 30, 30)
-  this.map.addTilesetImage('tileImage', 'tiles', 31, 31);
+TileMapper.prototype.createTiles = function(map_name, tile_image_name, tilesheet_name, collision_ranges) {
+  this.map = this.game.add.tilemap(map_name, 30, 30)
+  this.map.addTilesetImage(time_image_name, image_name, 31, 31);
   this.layer = this.map.createLayer(0);
   for (i = 0; i < collision_ranges.length; i++) {
     this.map.setCollisionBetween(collision_ranges[i][0],collision_ranges[i][1])
