dd12295555f7422b55603596d2b8c54fd4b55fac
Christian Stanfield <christian.stanfield@gmail.com>
2015-03-17 19:47:56 -0500

    upgraded db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141103031815) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "game_reports", force: true do |t|
     t.integer  "user_id"
     t.datetime "created_at"
